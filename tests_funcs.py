b3f5020d2a25f99c05d05264568df2ce3b309316
ADiallo1 <abdbirrare@gmail.com>
2021-12-22 15:34:04 +0100

    Red: minimum_int + tests

--- tests_funcs.py
+++ tests_funcs.py
@@ -4,10 +4,10 @@ import unittest
 class TestFuncs(unittest.TestCase):
 
 	def test_min(self):
-		self.assertEqual(funcs.max_int(0,2),2)
-		self.assertEqual(funcs.max_int(-1,-5),-1)
-		self.assertEqual(funcs.max_int(-1,2),2)
-		self.assertEqual(funcs.max_int(0,0),0)
+		self.assertEqual(funcs.minimum_int([10, 15, 5, 20, 16], 5)
+		self.assertEqual(funcs.minimum_int([3, 15, 5, 20, 16], 3)
+		self.assertEqual(funcs.minimum_int([10, 15, 5, 20, 2], 2)
+		self.assertEqual(funcs.minimum_int([10, 15, 5, -20, 16], -20)
 
 if __name__ == '__main__':
 	unittest.main()
