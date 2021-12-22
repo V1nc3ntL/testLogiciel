7abd9b7297d9a75877c34dc734c3bd432968ba95
Vincent <vega@localhost.localdomain>
2021-12-22 18:21:33 +0100

    refactor : add test option in functions

--- ex2/funcs.py
+++ ex2/funcs.py
@@ -4,7 +4,7 @@ import exdef
 
 cur = None
 
-def connect() :
+def connect(tst=True) :
     global cur 
     conn = sqlite3.connect(exdef.DB_NAME)
     try :
@@ -13,19 +13,21 @@ def connect() :
     except Exception as ex:
         return (False,ex)
 
-def create_table(table_name) :
+def create_table(table_name,tst=True) :
     # At least one row must be specified when creating a table
     cur.execute("create table "+table_name+"(s)")
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='"+table_name+"'") 
-    return cur.fetchall()[0][0]
+    if(tst):
+        cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='"+table_name+"'") 
+        return cur.fetchall()[0][0]
 
-def add_column(column_name,type_str="STRING") :
+def add_column(column_name,type_str="STRING",tst=True) :
     query = "ALTER TABLE "+exdef.TABLE_NAME + " ADD COLUMN " + column_name + " " + type_str
     cur.execute(query)
-    cols = cur.execute("SELECT name FROM PRAGMA_TABLE_INFO('"+exdef.TABLE_NAME+"')" ).fetchall()
-    if (column_name,) in cols :
-        return column_name 
-    else :
-        return "" 
+    if(tst):
+        cols = cur.execute("SELECT name FROM PRAGMA_TABLE_INFO('"+exdef.TABLE_NAME+"')" ).fetchall()
+        if (column_name,) in cols :
+            return column_name 
+        else :
+            return "" 
 
     
