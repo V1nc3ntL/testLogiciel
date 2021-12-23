8d79436a6282859b6c21bac549c198aaa488bab8
Vincent <vega@localhost.localdomain>
2021-12-23 15:06:39 +0100

    Refactor : import constants for less overhead

--- ex2/funcs.py
+++ ex2/funcs.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import sqlite3
-import exdef
+from exdef import  DB_NAME, TABLE_NAME, COLUMN_NAMES
 
 cur = None
 
 def connect(tst=True) :
     global cur 
-    conn = sqlite3.connect(exdef.DB_NAME)
+    conn = sqlite3.connect(DB_NAME)
     try :
         cur = conn.cursor()
         return (True,conn)
@@ -21,20 +21,20 @@ def create_table(table_name,tst=True) :
         return cur.fetchall()[0][0]
 
 def add_column(column_name,type_str="STRING",tst=True) :
-    query = "ALTER TABLE "+exdef.TABLE_NAME + " ADD COLUMN " + column_name + " " + type_str
+    query = "ALTER TABLE "+TABLE_NAME + " ADD COLUMN " + column_name + " " + type_str
     cur.execute(query)
     if(tst):
-        cols = cur.execute("SELECT name FROM PRAGMA_TABLE_INFO('"+exdef.TABLE_NAME+"')" ).fetchall()
+        cols = cur.execute("SELECT name FROM PRAGMA_TABLE_INFO('"+TABLE_NAME+"')" ).fetchall()
         if (column_name,) in cols :
             return column_name 
         else :
             return "" 
 
 def add_user(usr_name,password,tst=True) :
-    query = "INSERT INTO "+exdef.TABLE_NAME + "("+exdef.COLUMN_NAMES[0] + "," + exdef.COLUMN_NAMES[1] + ") values (?,?)" 
+    query = "INSERT INTO "+TABLE_NAME + "("+COLUMN_NAMES[0] + "," + COLUMN_NAMES[1] + ") values (?,?)" 
     cur.execute(query,(usr_name,password))
     if(tst):
-        query = "SELECT "+ exdef.COLUMN_NAMES[0] +", " +exdef.COLUMN_NAMES[1]+ "  FROM "+exdef.TABLE_NAME+" WHERE "+exdef.COLUMN_NAMES[0] + " = '" +usr_name +"'"
+        query = "SELECT "+ COLUMN_NAMES[0] +", " +COLUMN_NAMES[1]+ "  FROM "+TABLE_NAME+" WHERE "+COLUMN_NAMES[0] + " = '" +usr_name +"'"
         fields = cur.execute(query).fetchall()
         return fields[0]
     
--- ex2/test_func.py
+++ ex2/test_func.py
@@ -2,7 +2,7 @@
 import funcs
 import unittest
 import sqlite3
-import exdef
+from  exdef import TABLE_NAME, COLUMN_NAMES, WORKING_USERS
 
 class TestFuncs(unittest.TestCase):
 
@@ -16,19 +16,19 @@ class TestFuncs(unittest.TestCase):
 
     def test_01_table_creation(self):
         # Compare the name of tables
-        connected = self.assertEqual(funcs.create_table(exdef.TABLE_NAME),exdef.TABLE_NAME)
+        connected = self.assertEqual(funcs.create_table(TABLE_NAME),TABLE_NAME)
         if not connected :
             # Ensure that the tests won't go further if no table can be created
             return -1    
 
     def test_02_add_column(self):
         # Compare the name of columns
-        for col in exdef.COLUMN_NAMES:
+        for col in COLUMN_NAMES:
             self.assertEqual(funcs.add_column(col),col)
 
     # Test for the user add function
     def test_03_add_user(self):
-        for usr in exdef.WORKING_USERS:
+        for usr in WORKING_USERS:
             self.assertEqual(funcs.add_user(usr[0],usr[1]),(usr[0],usr[1]))
 
 if __name__ == '__main__':
